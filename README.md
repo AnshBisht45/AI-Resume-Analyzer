96ab8b267a5e26ed311d0072c1d4d6a102e59159
AnshBisht45 <ansh.24bai10828@vitbhopal.ac.in>
2026-05-12 21:29:14 +0530

    Initial AI Resume Analyzer project

---
+++ README.md
@@ -0,0 +1,30 @@
+# AI Resume Analyzer
+
+An AI-powered Resume Analyzer built using React and FastAPI.
+
+## Features
+- Resume PDF Upload
+- Skill Extraction
+- ATS Score Calculation
+- Modern UI
+- Full Stack Project
+
+## Tech Stack
+- React
+- FastAPI
+- Python
+- PDFPlumber
+
+## Future Improvements
+- Job Description Matching
+- AI Recommendations
+- Resume Feedback
+- NLP Enhancements
+
+## Screenshots
+
+### Upload UI
+![Upload UI](screenshots/upload-ui.png)
+
+### Result UI
+![Result UI](screenshots/result-ui.png)
---
+++ backend/main.py
@@ -0,0 +1,70 @@
+from fastapi.middleware.cors import CORSMiddleware
+import pdfplumber
+from fastapi import FastAPI, UploadFile, File
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+skills_list = [
+    "mysql",
+    "postgresql",
+    "database",
+    "python",
+    "java",
+    "c++",
+    "react",
+    "sql",
+    "machine learning",
+    "fastapi",
+    "tensorflow",
+    "html",
+    "css",
+    "javascript",
+    "node.js"
+]
+
+@app.get("/")
+def home():
+    return {"message": "AI Resume Analyzer Running"}
+
+@app.post("/upload")
+async def upload_resume(file: UploadFile = File(...)):
+
+    text = ""
+
+    with pdfplumber.open(file.file) as pdf:
+
+        for page in pdf.pages:
+
+            extracted = page.extract_text()
+
+            if extracted:
+                text += extracted
+
+    lower_text = text.lower()
+
+    found_skills = []
+
+    for skill in skills_list:
+
+        if skill.lower() in lower_text:
+
+            found_skills.append(skill)
+
+    ats_score = len(found_skills) * 10
+
+    if ats_score > 100:
+        ats_score = 100
+
+    return {
+        "skills_found": found_skills,
+        "ats_score": ats_score,
+        "resume_text": text
+    }
